d10ad514012b4f6f3015cff93c14d2da8110e17d
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-02-04 18:52:40 +0600

    add: generate Static page

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,6 +1,22 @@
 import BlogDetailsCard from "@/components/ui/BlogDetailsCard";
 
+export const generateStaticParams = async () => {
+    return [
+        {
+            blogId:"1",
+        },
+        {
+            blogId:"2",
+        },
+        {
+            blogId:"3",
+        }
+    ]
+}
+
+
 const BlogDetailsPage = async({params}:{params:Promise<{blogId:string}>}) => {
+
     const blogId = (await params).blogId;
     const res = await fetch(`http://localhost:5000/blogs/${blogId}`,{
         next:{
