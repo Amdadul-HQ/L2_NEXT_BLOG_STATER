50bf98a4e4b0685e6eccf7b9b17f9ca10c3ca9dc
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-02-04 18:22:13 +0600

    add: blog page seo

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,13 +1,21 @@
 import BlogCard from "@/components/ui/BlogCard";
 import { Blog } from "@/types";
 
+export const metadata = {
+    title:"NexaBlog | Blog",
+    description:"this is a blog page"
+}
+
+
 const BlogPage = async() => {
-    const res = await fetch("http://localhost:5000/blogs");
+    const res = await fetch("http://localhost:5000/blogs",{
+        cache:"no-store"
+    });
     const blogs = await res.json();
     return (
-        <div>
+        <div className="container mx-auto">
              <h1 className="text-3xl text-center my-5 font-bold">
-        Explore All<span className="text-teal-600">Blog</span>
+        Explore All <span className="text-teal-600">Blog</span>
       </h1>
       <p className="text-center text-gray-400 w-2/5 mx-auto">
         <i>
@@ -15,7 +23,7 @@ const BlogPage = async() => {
           unprecedented computational power.
         </i>
       </p>
-      <div>
+      <div className="grid grid-cols-3 gap-5 my-5">
         {blogs.map((blog:Blog) => <BlogCard key={blog.id} blog={blog}/>)}
       </div>
         </div>
