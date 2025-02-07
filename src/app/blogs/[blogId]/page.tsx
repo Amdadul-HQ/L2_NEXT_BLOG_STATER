17202bc136e382223bd0651a351a57927771d17a
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-02-07 19:50:35 +0600

    add: meta title and description

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -10,6 +10,19 @@ export const generateStaticParams = async () => {
     }))
 }
 
+export const generateMetadata = async({params}:{params:Promise<{blogId:string}>}) => {
+    const blogId = await params.blogId;
+    const res = await fetch(`http://localhost:5000/blogs/${blogId}`,{
+        next:{
+            revalidate:30
+        }
+    });
+    const blog = await res.json();
+    return {
+        title:blog.title,
+        description:blog.description
+    }
+}
 
 const BlogDetailsPage = async({params}:{params:Promise<{blogId:string}>}) => {
 
